9beebb984d3ad96168d73fe968cb8dfe9648a1c2
artanton <anton.kononov.dev@gmail.com>
2025-07-18 23:48:45 +0300

    added link to instagramm with animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import { useState } from 'react';
-import './App.css';
-import Box from '@mui/material/Box';
-import Input from './components/input/Input.tsx';
-import type { Imatrix } from './interface.ts';
-import Chart from './components/charts/Chart.tsx';
+import { useState } from "react";
+import "./App.css";
+import Box from "@mui/material/Box";
+import Input from "./components/input/Input.tsx";
+import type { Imatrix } from "./interface.ts";
+import Chart from "./components/charts/Chart.tsx";
 
 function App() {
   const [matrix, setMatrix] = useState<Imatrix | null>(null);
   return (
     <Box
-    sx={{
-      padding: '30px',
-      borderRadius: 4,
-      backgroundColor: 'white',
-           
-    }}>
-    <h1 className = "app-title">Розрахунок матриці</h1>
-   <Input onResult = {setMatrix}/>
-   <Chart matrix = {matrix}/>
-   <p> Замовити розбір </p>
+      sx={{
+        padding: "30px",
+        borderRadius: 4,
+        backgroundColor: "white",
+      }}
+    >
+      <h1 className="app-title">Розрахунок матриці</h1>
+      <Input onResult={setMatrix} />
+      <Chart matrix={matrix} />
+      <p>
+        <a
+          href="https://www.instagram.com/psiho_kod/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Замовити розбір
+        </a>
+      </p>
     </Box>
-  )
+  );
 }
 
-export default App
+export default App;
